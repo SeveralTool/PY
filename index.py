96080148f80b0272df21b7ad4b8bc281f8a6ea16
Nahuel Galeano <nahuelgaleano7778ng7@gmail.com>
2022-11-28 08:48:42 -0300

    hh

--- index.py
+++
@@ -1,3 +0,0 @@
-
-
-print ("Que andas la concha de tu madresaDFASDF :)")
---
+++ notas.py
@@ -0,0 +1,8 @@
+
+# print ("Que andas la concha de tu madre :) ")
+
+num = 2
+
+if (num < 10) : print("ajsjasjajs")
+else : print("tu madree")
+
