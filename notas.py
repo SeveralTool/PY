b59cda66be2fcdc314fb6ef3343b9b9c44f9091e
Nahuel Galeano <nahuelgaleano7778ng7@gmail.com>
2022-11-29 09:46:16 -0300

    ok

--- notas.py
+++ notas.py
@@ -1,9 +1,57 @@
 
 # print ("Que andas la concha de tu madre :) ")
+# print (str(3)) #para pasar de num a string con str
 
-num = 2
+# //////////////////////////////
 
-if (num < 10) : print("ajsjasjajs")
-else : print("tu madree")
+# my_float = 2.5 * 3
+# print(int(my_float)) pasamos de float a entero con int
+# print(len("holaaa")) contar caracteres con len
+# operadores logicos son : and/or/not
 
-adsf
+# //////////////////////////////
+
+#poner variables en una cadena de texto con {} , poniendo f al principio
+# name, apellido, edad = "Nahuel", "Galeano", 23
+# print(f"Mi nombre es {name} y mi apellido {apellido}, tengo {edad} años.")
+
+# //////////////////////////////
+
+# Poner palabras al reves
+
+# nom = "Nahuel"
+# rev_nom = nom[::-1]
+# print(rev_nom)
+
+# //////////////////////////////
+
+# Funciones
+
+# .count() = cuenta cuantos caracteres
+# .isnumeric() = verifica si es numero
+# .lower() = pasa a minuscula
+# .upper() = pasa a mayuscula
+
+# //////////////////////////////
+
+# Listas
+
+my_lista = [23,234,24, "Nahuel",23]
+my_other_list = list()
+
+print(my_lista[1])
+
+
+# //////////////////////////////
+
+
+# //////////////////////////////
+
+
+# //////////////////////////////
+
+
+# num = 2
+
+# if (num < 10) : print("ajsjasjajs")
+# else : print("tu madree")
