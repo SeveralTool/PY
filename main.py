9221373d77a95b264688d5cc4cea41971036d15e
Nahuel Galeano <nahuelgaleano7778ng7@gmail.com>
2023-01-24 17:43:46 -0300

    update

--- main.py
+++ main.py
@@ -5,15 +5,19 @@
 #   pip install "fastapi[all]" # --user si no funciona al final 
 #   pip install "uvicorn[standard]"
 #   uvicorn main:app --reload | or | python -m uvicorn main:app --reload
-#
+
+#   url/docs # para ver al documentacion automatica del API
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/") # url
 async def root():
     return "Hola FastAPI"
 
-
+@app.get("/github") # url/github
+async def root():
+    return "https://github/severaltool"
 
 
